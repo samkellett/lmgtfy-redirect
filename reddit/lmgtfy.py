41d0298cef31779e82ca7aae039c564e3aeb4281
Sam Kellett <samkellett@gmail.com>
2014-08-03 19:58:09 +0100

    Added a cache for old comments and a regular expression to match all lmgtfy-esque links

--- reddit/lmgtfy.py
+++ reddit/lmgtfy.py
@@ -1,6 +1,8 @@
 import praw
+import re
 import sys
-# from collections import deque
+
+from collections import deque
 
 try:
   import lmgtfy_bot as bot
@@ -8,11 +10,21 @@ except ImportError, e:
   print "No username and password found!"
   sys.exit(10)
 
-# cache = deque(maxlen = 200)
-
 def main():
   reddit = praw.Reddit(user_agent = bot.USERAGENT)
   reddit.login(bot.USERNAME, bot.PASSWORD)
+
+  links = re.compile('https?://(www\.)?((((en|ru|de|pt-br|fr|pl|zh-tw|nl|es|images|maps|video|news|shopping|photos|plus|profiles|books|finance|scholar|bing|snopes|wikipedia)\.)?lmgtfy|lm(b|sp|s)tfy)\.com|lmddgtfy.net)')
+  cache = deque(maxlen = 200)
+
+  running = True
+  while running:
+    comments = reddit.get_all_comments(limit = None, url_data = {'limit': 100})
+    for comment in comments:
+      if comment.id in cache:
+        break
+
+      cache.append(comment.id)
 
   print bot.USERNAME
 
