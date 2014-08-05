f73f82c5ed88c3b47c680c4ed26dc37ce0189897
Sam Kellett <samkellett@gmail.com>
2014-08-05 19:31:01 +0100

    Fixed the regular expression and increased the request count to avoid missing comments

--- reddit/lmgtfy.py
+++ reddit/lmgtfy.py
@@ -1,8 +1,10 @@
 import praw
-import regex
+import re
 import sys
 
 from collections import deque
+from functools import partial
+from time import sleep
 
 try:
   import lmgtfy_bot as bot
@@ -10,25 +12,39 @@ except ImportError, e:
   print "No username and password found!"
   sys.exit(10)
 
+filter_empty = partial(filter, None)
+
 def main():
   reddit = praw.Reddit(user_agent = bot.USERAGENT)
   reddit.login(bot.USERNAME, bot.PASSWORD)
 
-  links = regex.compile('(?|(?<txt>(?<url>https?://(www\.)?((((en|ru|de|pt-br|fr|pl|zh-tw|nl|es|images|maps|video|news|shopping|photos|plus|profiles|books|finance|scholar|bing|snopes|wikipedia)\.)?lmgtfy|lm(b|sp|s)tfy)\.com|lmddgtfy.net)))|\(([^)]+)\)\[(\g<url>)\])')
-  cache = deque(maxlen = 200)
+  url = 'https?://(?:www\.)?(?:(?:(?:(?:en|ru|de|pt-br|fr|pl|zh-tw|nl|es|images|maps|video|news|shopping|photos|plus|profiles|books|finance|scholar|bing|snopes|wikipedia)\.)?lmgtfy|lm(?:b|sp|s)tfy)\.com|lmddgtfy\.net)/?\??[^\s\]]*'
+  links = re.compile('(?:\(([^)]+)\)\[(' + url + ')\])|((' + url + '))')
+  cache = deque(maxlen = 2000)
 
   running = True
   while running:
-    comments = reddit.get_comments('all', limit = 100)
+    comments = reddit.get_comments('all', limit = 1000)
     for comment in comments:
       if comment.id in cache:
-        break
+        continue
 
       cache.append(comment.id)
-      print (comment.body)
-      running = False
+      print comment.body
 
-  print bot.USERNAME
+      found_links = map(filter_empty, re.findall(links, comment.body))
+      if found_links:
+        try:
+          for text, query in found_links:
+            print "FOUND:", text, query
+            exit(100)
+
+        except praw.errors.APIException, e:
+          print "[ERROR]:", e
+          print "sleeping for 30 sec..."
+          sleep(30)
+
+    sleep(2)
 
 if __name__ == '__main__':
   main()
